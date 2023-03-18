9e36c18e4e4905cb3951d9ebb28bd9d2a8a7ab9a
Andrey-Strg <mr.kpbis@gmail.com>
2023-03-18 20:28:38 +0400

    feat: save rates to file

---
+++ src/exchange/constants.ts
@@ -0,0 +1,2 @@
+export const EXCHANGES_FILE_NAME = 'exchanges_save.json';
+export const CRON_SCHEDULE = '0 * */10 * *';
--- src/exchange/index.ts
+++ src/exchange/index.ts
@@ -1,7 +1,8 @@
 import * as cron from 'node-cron';
+import * as fs from 'fs';
 import { requestCurrencies, requestLatest } from '../openexchange-client/index.js';
 import { CalculateDiffResult, ConvertAmountResult, CurrenciesListItem } from './interfaces.js';
-import { CRON_SCHEDULE } from '../constants.js';
+import { CRON_SCHEDULE, EXCHANGES_FILE_NAME } from './constants.js';
 
 export class ExchangeDiff {
   private currenciesList: Map<string, CurrenciesListItem> = new Map();
@@ -31,7 +32,7 @@ export class ExchangeDiff {
     };
   }
 
-  public getAll() {
+  public getAll(): Array<[string, CurrenciesListItem]> {
     return Array.from(this.currenciesList.entries());
   }
 
@@ -53,13 +54,22 @@ export class ExchangeDiff {
   }
 
   private async init() {
-    await this.updateCurrencies();
+    const data = await this.readFromFile();
+    if (data.length === 0) {
+      await this.updateCurrencies();
+    } else {
+      console.log('currencies loaded from file...');
+      const list: [string, CurrenciesListItem][] = JSON.parse(data);
+      for(const item of list) {
+        this.currenciesList.set(item[0], item[1]);
+      }
+    }
+    
     cron.schedule(CRON_SCHEDULE, async () => { await this.updateCurrencies();});
   }
 
   private async updateCurrencies(): Promise<void>  {
-    console.log('loading currencies...');
-    console.log(new Date);
+    console.log('requesting currencies...');
     const currenciesResponse = await requestCurrencies();
     const USDRates = await requestLatest();
     const codes = Object.keys(currenciesResponse);
@@ -69,5 +79,30 @@ export class ExchangeDiff {
         rate: USDRates.rates[code],
       });
     });
+    const data = this.getAll();
+    this.writeToFile(JSON.stringify(data));
+  }
+
+  private writeToFile(data: string): void {
+    const writableStream = fs.createWriteStream(EXCHANGES_FILE_NAME);
+    writableStream.write(data);
+    writableStream.close();
+  }
+
+  private async readFromFile(): Promise<string> {
+    let result = '';
+    const readableStream = fs.createReadStream(EXCHANGES_FILE_NAME, 'utf-8');
+    try {
+      for await (const chunk of readableStream) {
+        result += chunk;
+      }
+    } 
+    catch (error) {
+      console.log(error.message);
+    }
+    finally {
+      readableStream.close();
+    }
+    return result;
   }
 }
